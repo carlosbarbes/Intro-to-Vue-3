00c5c18c1e351e2bba279e2cd992980f06837179
Carlos Bárcena Bescansa <carlos.barcena.bescansa@gmail.com>
2023-11-12 21:21:41 +0100

    image and url added

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'Blue warm socks for winter time.'
+      description: 'Blue warm socks for winter time.',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://www.vuemastery.com/'
     }
   }
 });
