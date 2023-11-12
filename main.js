edfea3ae6da307dc6cf3d351283a8d5372ebbc66
Carlos Bárcena Bescansa <carlos.barcena.bescansa@gmail.com>
2023-11-13 00:14:03 +0100

    mouseover and add to cart socks

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const app = Vue.createApp({
       description: 'Blue warm socks for winter time.',
       image: './assets/images/socks_blue.jpg',
       inventory: 5,
-      onSale: false
+      onSale: false,
+      details: ['50% cotton','30% wool','20% polyester'],
+      variants: [
+        { id: 2234, color: 'green'},
+        { id: 2235, color: 'blue'}
+      ]
     }
   }
 });
